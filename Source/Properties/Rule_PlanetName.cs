884a8c02393eb64147338b07c9481cb9e94bc29a
jecrell <matt.walls31@gmail.com>
2017-05-20 23:39:20 +0900

    A17 - New Rebel / Imperial icons and Rebel name generator

---
+++ Source/Properties/Rule_PlanetName.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using System;
+using Verse;
+using Verse.Grammar;
+
+namespace SWFactions
+{
+    public class Rule_PlanetName : Rule
+    {
+        public int selectionWeight = 1;
+        
+        public override float BaseSelectionWeight
+        {
+            get
+            {
+                return (float)this.selectionWeight;
+            }
+        }
+
+        public override string Generate()
+        {
+            return Find.World.info.name;
+        }
+
+        public override string ToString()
+        {
+            return this.keyword + "->(worldname)";
+        }
+    }
+}
