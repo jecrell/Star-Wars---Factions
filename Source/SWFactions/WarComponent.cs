bf377f32a23cde76ee7a455e06d8ca747ad7c5b2
jecrell <matt.walls31@gmail.com>
2018-08-21 00:01:38 +0900

    Fixes for 0.19.1998

--- Source/SWFactions/WarComponent.cs
+++ Source/SWFactions/WarComponent.cs
@@ -14,25 +14,28 @@ namespace SWFactions
 
         public WarComponent(World world) : base(world)
         {
-
         }
 
         public override void WorldComponentTick()
         {
             base.WorldComponentTick();
             if (Find.TickManager.TicksGame % 100 == 0 &&
-                !hostilityDeclared &&
-                Find.FactionManager.FirstFactionOfDef(FactionDef.Named("PJ_RebelFac")) is Faction rebelFaction &&
-                Find.FactionManager.FirstFactionOfDef(FactionDef.Named("PJ_GalacticEmpire")) is Faction impFaction)
+                !hostilityDeclared)
             {
                 hostilityDeclared = true;
-                impFaction.TrySetNotAlly(rebelFaction, true);
-                rebelFaction.TrySetNotAlly(impFaction, true);
-                Find.LetterStack.ReceiveLetter("PJ_WarDeclared".Translate(), "PJ_WarDeclaredDesc".Translate(
-                    new object[] { rebelFaction.def.label, impFaction.def.label }
+                if (Find.FactionManager.FirstFactionOfDef(DefDatabase<FactionDef>.GetNamedSilentFail("PJ_RebelFac")) is
+                        Faction rebelFaction &&
+                    Find.FactionManager.FirstFactionOfDef(
+                            DefDatabase<FactionDef>.GetNamedSilentFail("PJ_GalacticEmpire"))
+                        is Faction impFaction)
+                {
+                    impFaction.TrySetNotAlly(rebelFaction, true);
+                    rebelFaction.TrySetNotAlly(impFaction, true);
+                    Find.LetterStack.ReceiveLetter("PJ_WarDeclared".Translate(), "PJ_WarDeclaredDesc".Translate(
+                        new object[] {rebelFaction.def.label, impFaction.def.label}
                     ), DefDatabase<LetterDef>.GetNamed("PJ_BadUrgent"), null);
+                }
             }
-
         }
 
         public override void ExposeData()
