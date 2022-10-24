a543ec69699adbf5acf7f8dd2b4f6e3996fae732
Matthew Walls <matt.walls31@gmail.com>
2022-10-24 12:04:22 -0700

    Mlie Import & Ideology Updates
    Loaded mlie's updates
    Added ideologies for factions
    - Imperials are supremacist, raider, collectivist proselytizers
    - Rebels are loyalist, individualist, proselytizers
    Added compativility with Star Wars Animal Collection (1.3 and 1.4 when it's updated)
    - Imperials have chances of dewbacks when trading
    - Rebels have chances of Tauntauns when trading
    - Scum have chances of rancors, reeks, and nexu to be included in their raiding parties and settlements

---
+++ Source/SWFactions_Utility.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace SWSaber
+{
+    public static class Utility
+    {
+        public static bool modCheck = false;
+        public static bool loadedForcePowers = false;
+        public static bool loadedLightsabers = false;
+        //public static bool loadedFactions = false;
+
+        public static bool AreForcePowersLoaded()
+        {
+            if (!modCheck) ModCheck();
+            return loadedForcePowers;
+        }
+        //public static bool AreFactionsLoaded()
+        //{
+        //    if (!modCheck) ModCheck();
+        //    return loadedFactions;
+        //}
+        public static bool AreLightsabersLoaded()
+        {
+            if (!modCheck) ModCheck();
+            return loadedLightsabers;
+        }
+
+        public static void ModCheck()
+        {
+            Log.Message(text: "Mod Check Called");
+            loadedForcePowers = false;
+            loadedLightsabers = false;
+            //loadedFactions = false;
+            foreach (ModContentPack ResolvedMod in LoadedModManager.RunningMods)
+            {
+                if (loadedForcePowers && loadedLightsabers) break; //Save some loading
+                if (ResolvedMod.Name.Contains(value: "Star Wars - The Force"))
+                {
+                    Log.Message(text: "Lightsabers :: Star Wars - The Force Detected.");
+                    loadedForcePowers = true;
+                }
+                //if (ResolvedMod.Name.Contains("Star Wars - Factions"))
+                //{
+                //    Log.Message("Lightsabers :: Star Wars - Factions Detected.");
+                //    loadedFactions = true;
+                //}
+                if (ResolvedMod.Name.Contains(value: "Star Wars - Fully Functional Lightsabers"))
+                {
+                    Log.Message(text: "Lightsabers :: Star Wars - Lightsabers.");
+                    loadedLightsabers = true;
+                }
+            }
+            modCheck = true;
+            return;
+        }
+    }
+}
---
+++ Source/WarComponent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using RimWorld.Planet;
+
+namespace SWFactions
+{
+    public class WarComponent : WorldComponent
+    {
+        public bool hostilityDeclared = false;
+
+        public WarComponent(World world) : base(world: world)
+        {
+        }
+
+        public override void WorldComponentTick()
+        {
+            base.WorldComponentTick();
+            if (Find.TickManager.TicksGame % 500 == 0 &&
+                !hostilityDeclared)
+            {
+                hostilityDeclared = true;
+                if (Find.FactionManager.FirstFactionOfDef(facDef: DefDatabase<FactionDef>.GetNamedSilentFail(defName: "PJ_RebelFac")) is { } rebelFaction &&
+                    Find.FactionManager.FirstFactionOfDef(
+                            facDef: DefDatabase<FactionDef>.GetNamedSilentFail(defName: "PJ_GalacticEmpire"))
+                        is { } impFaction)
+                {
+                    impFaction.SetRelationDirect(other: rebelFaction,kind: FactionRelationKind.Hostile,canSendHostilityLetter: false);
+                    rebelFaction.SetRelationDirect(other: impFaction, kind: FactionRelationKind.Hostile, canSendHostilityLetter: false);
+
+                    Find.MusicManagerPlay.disabled = true;
+                    Find.MusicManagerPlay.ForceSilenceFor(time: 10f);
+                    Find.MusicManagerPlay.disabled = false;
+                    Find.LetterStack.ReceiveLetter(label: "PJ_WarDeclared".Translate(),   
+                        text: "PJ_WarDeclaredDesc".Translate(rebelFaction.def.label, impFaction.def.label)
+                    , textLetterDef: DefDatabase<LetterDef>.GetNamed(defName: "PJ_BadUrgent"), debugInfo: null);
+                }
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look<bool>(value: ref this.hostilityDeclared, label: "hostilityDeclared", defaultValue: false);
+        }
+    }
+}
